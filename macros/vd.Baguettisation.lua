fb883b427b450524dddfa6080a743905548b8d52
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-06-18 04:54:54 +0200

    push version 1.7.5

--- macros/vd.Baguettisation.lua
+++ macros/vd.Baguettisation.lua
@@ -1,6 +1,6 @@
 script_name = "Baguettisation"
 script_description = "Handle dialogue stuff"
-script_version = "1.7.4"
+script_version = "1.7.5"
 script_author = "Vardë"
 script_namespace = "vd.Baguettisation"
 
